e6c07b4a9a80ee2cc0eae4225a96e35193c400af
Filip Troníček <filip.tronicek@seznam.cz>
2019-11-17 19:44:37 +0100

    Install mousetrap and add Ctr+C command

--- src/clip.js
+++ src/clip.js
@@ -1,5 +1,6 @@
 const electron = require("electron");
 const path = require("path");
+const Mousetrap = require('mousetrap');
 const { clipboard } = require("electron");
 
 function isKeyPressed(event, expectedKey, expectedCode) {
@@ -21,19 +22,25 @@ function sendURL() {
     // receive message from main.js
     ipcRenderer.on("asynchronous-reply", (event, code) => {
       if(code != "") {
-          document.getElementById("code").innerHTML = "Code: "+code;
-          clipboard.writeText(code, "selection");
-          console.log(`Copied: ${clipboard.readText("selection")}`);
+          document.getElementById("code").innerHTML = "Code: <span id='theCode'>"+code+"</span>";
       }
     });
 }
 
+Mousetrap.bind(['command+c', 'ctrl+c'], function() {
+    clipboard.writeText(document.getElementById("theCode").innerHTML, "selection");
+    console.log(`Copied: ${clipboard.readText("selection")}`);
+    // return false to prevent default browser behavior
+    // and stop event from bubbling
+    return false;
+});
 document
   .getElementById("search-input")
   .addEventListener("keydown", function(event) {
     if (isKeyPressed(event, "Enter", 13)) {
         event.preventDefault();
         sendURL();
+    } else {
     }
   });
   var expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gi;
