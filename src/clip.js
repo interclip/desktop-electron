0451854763b0640622e7b144bb3714ab9698f749
Filip Troníček <filip.tronicek@seznam.cz>
2019-11-17 14:09:13 +0100

    Show code in HTML

--- src/clip.js
+++ src/clip.js
@@ -1,19 +1,36 @@
-const electron = require('electron');
-const path = require('path');
+const electron = require("electron");
+const path = require("path");
+const { clipboard } = require("electron");
 
-document.querySelector('#submit').addEventListener('click', function() {
+function isKeyPressed(event, expectedKey, expectedCode) {
+  const code = event.which || event.keyCode;
 
-    let username = document.getElementById("search-input").value;
+  if (expectedKey === event.key || code === expectedCode) {
+    return true;
+  }
+  return false;
+}
 
-    const {ipcRenderer} = require('electron');
+document
+  .getElementById("search-input")
+  .addEventListener("keydown", function(event) {
+    if (isKeyPressed(event, "Enter", 13)) {
+      event.preventDefault();
+      console.log("enter was pressed and is prevented");
 
-    // send username to main.js 
-    ipcRenderer.send('asynchronous-message', username )
+      let username = document.getElementById("search-input").value;
 
-    // receive message from main.js
-    ipcRenderer.on('asynchronous-reply', (event, arg) => {
-      console.log(arg);
+      const { ipcRenderer } = require("electron");
 
-    });
+      // send username to main.js
+      ipcRenderer.send("asynchronous-message", username);
 
-    });
+      // receive message from main.js
+      ipcRenderer.on("asynchronous-reply", (event, code) => {
+        console.log(code);
+        document.getElementById("code").innerHTML = "Code: "+code;
+        clipboard.writeText(code, "selection");
+        console.log(`Copied: ${clipboard.readText("selection")}`);
+      });
+    }
+  });
