342234a4123dcd3caa0ae8d2aea0244f1a2302a0
Filip Troníček <filip.tronicek@seznam.cz>
2019-11-18 16:50:12 +0100

    Some stuff

--- src/clip.js
+++ src/clip.js
@@ -1,6 +1,6 @@
 const electron = require("electron");
 const path = require("path");
-const Mousetrap = require('mousetrap');
+const Mousetrap = require("mousetrap");
 const { clipboard } = require("electron");
 
 function isKeyPressed(event, expectedKey, expectedCode) {
@@ -11,47 +11,50 @@ function isKeyPressed(event, expectedKey, expectedCode) {
   }
   return false;
 }
-function sendURL() {
-    let username = document.getElementById("search-input").value;
+function sendURL(urlInput = document.getElementById("search-input").value) {
+  const { ipcRenderer } = require("electron");
 
-    const { ipcRenderer } = require("electron");
+  // send username to main.js
+  ipcRenderer.send("asynchronous-message", urlInput);
 
-    // send username to main.js
-    ipcRenderer.send("asynchronous-message", username);
-
-    // receive message from main.js
-    ipcRenderer.on("asynchronous-reply", (event, code) => {
-      if(code != "") {
-          document.getElementById("code").innerHTML = "Code: <span id='theCode'>"+code+"</span>";
-      }
-    });
+  // receive message from main.js
+  ipcRenderer.on("asynchronous-reply", (event, code) => {
+    if (code != "") {
+      document.getElementById("code").innerHTML =
+        "Code: <span id='theCode'>" + code + "</span>";
+    }
+  });
 }
 
-Mousetrap.bind(['command+c', 'ctrl+c'], function() {
-    clipboard.writeText(document.getElementById("theCode").innerHTML, "selection");
-    console.log(`Copied: ${clipboard.readText("selection")}`);
-    // return false to prevent default browser behavior
-    // and stop event from bubbling
-    return false;
+Mousetrap.bind(["command+c", "ctrl+c"], function() {
+  clipboard.writeText(
+    document.getElementById("theCode").innerHTML,
+    "selection"
+  );
+  console.log(`Copied: ${clipboard.readText("selection")}`);
+  // return false to prevent default browser behavior
+  // and stop event from bubbling
+  return false;
 });
 document
   .getElementById("search-input")
   .addEventListener("keydown", function(event) {
     if (isKeyPressed(event, "Enter", 13)) {
-        event.preventDefault();
-        sendURL();
+      event.preventDefault();
+      sendURL();
     } else {
     }
   });
-  var expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gi;
-  var regex = new RegExp(expression);
+var expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gi;
+var regex = new RegExp(expression);
+
+
+document.getElementById("body").onfocus = function() {
+  console.log("Focus");
   var t = clipboard.readText();
   if (t.match(regex)) {
-    alert("Successful match");
-  } else {
-    alert("No match");
-  }
-  
-  document.getElementById("body").onfocus = function(){console.log("Focus");document.getElementById("search-input").value = clipboard.readText();;sendURL()}; 
-  
+    sendURL(t);
+    document.getElementById("search-input").value = clipboard.readText();
 
+  } 
+};
