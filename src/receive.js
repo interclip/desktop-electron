f15fa02c07a5488f41b0149820d0c23adf82559e
Filip Troníček <filip.tronicek@seznam.cz>
2019-11-19 23:07:51 +0100

    Receive frontend

---
+++ src/receive.js
@@ -0,0 +1,4 @@
+const electron = require("electron");
+const path = require("path");
+const Mousetrap = require("mousetrap");
+const { clipboard, remote, ipcRenderer, dialog } = require("electron");
