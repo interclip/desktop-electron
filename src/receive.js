aceecd4a8ce5e91b2d2275fefe0a4c2a360abf0a
Filip Troníček <filip.tronicek@seznam.cz>
2019-11-20 16:35:28 +0100

    Fix input text size and add animation

--- src/receive.js
+++ src/receive.js
@@ -2,3 +2,27 @@ const electron = require("electron");
 const path = require("path");
 const Mousetrap = require("mousetrap");
 const { clipboard, remote, ipcRenderer, dialog } = require("electron");
+
+codeInput = document.getElementById("code");
+
+function placeHolder() {
+    var hash = Math.random()
+      .toString(36)
+      .substr(2, 5);
+  
+    if (codeInput.setAttribute("placeholder", hash)) {
+      console.log("Placed holder");
+    } else {
+      console.log("fuck me");
+    }
+  }
+
+  setInterval(function() {
+    placeHolder();
+  }, 50);
+
+document.getElementById("body").onfocus = function() {
+    //console.log("Focus");
+    codeInput.focus();
+    getClipboard();
+};
