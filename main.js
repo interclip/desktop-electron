13b6144b8c2362c8d562a6f66fed3e6a25143afa
Filip Troníček <filip.tronicek@seznam.cz>
2020-09-28 00:44:39 +0200

    Don't console.log()

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const {
   BrowserWindow,
   Menu,
   dialog,
-  globalShortcut,
   ipcMain,
 } = require("electron");
 
@@ -80,7 +79,6 @@ function createWindow() {
 
   // receive message from index.html
   ipcMain.on("asynchronous-message", (event, url) => {
-    console.log(url);
     axios.get(`http://uni.hys.cz/includes/api?url=${url}`).then((res) => {
       const code = res.data;
       // send message to index.html
@@ -88,7 +86,6 @@ function createWindow() {
     });
   });
   ipcMain.on("recieve-code", (event, code) => {
-    console.log(code);
     axios
       .get(`http://uni.hys.cz/includes/get-api?user=${code}`)
       .then((res) => {
