0ae848ac640ff69caeda779369c171ca4451b89b
Filip Troníček <filip.tronicek@seznam.cz>
2021-07-13 20:49:32 +0200

    Dark mode!

--- renderer/components/Menu.tsx
+++ renderer/components/Menu.tsx
@@ -7,7 +7,7 @@ const MenuItem = (props: {
 }): JSX.Element => {
   return (
     <Link href={props.href}>
-      <li className="px-4 py-4 cursor-pointer list-none bg-white rounded-xl">
+      <li className="menu-item px-4 py-4 cursor-pointer list-none bg-white rounded-xl dark:text-white">
         {props.children}
       </li>
     </Link>
@@ -47,11 +47,12 @@ const Menu = (): JSX.Element => {
               href={`https://github.com/interclip/desktop/issues/new?title=bug:&body=${encodeURIComponent(
                 debugInfo
               )}`}
-              className="px-4 py-4 cursor-pointer list-none bg-white rounded-xl"
+              className="menu-item px-4 py-4 cursor-pointer list-none bg-white rounded-xl"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="w-6"
+                fill="currentColor"
                 viewBox="0 0 512 512"
               >
                 <path d="M463.55 272.13H400v-48.2q0-4.32-.27-8.47c29.57-27.88 32.25-64.63 32.27-103 0-8.61-6.64-16-15.25-16.41A16 16 0 00400 112c0 28-1.86 48.15-9.9 63.84-19.22-41.15-65.78-63.91-134.1-63.91-39.8 0-74.19 9.13-99.43 26.39-14.9 10.19-26.2 22.91-33.7 37.72C114 160.65 112 141 112 112.46c0-8.61-6.6-16-15.2-16.44A16 16 0 0080 112c0 37.63 2.61 73.73 32.44 101.63q-.43 5.06-.44 10.3v48.2H48.45c-8.61 0-16 6.62-16.43 15.23a16 16 0 0016 16.77h64V320a143.32 143.32 0 0010.39 53.69C96.74 396.64 80.18 422 80 463.34c0 8.74 6.62 16.3 15.36 16.65A16 16 0 00112 464c0-27.66 9.1-44.71 26.17-61.32A144.37 144.37 0 00220 459.42a16 16 0 0020-15.49V192.45c0-8.61 6.62-16 15.23-16.43A16 16 0 01272 192v251.93a16 16 0 0020 15.49 144.4 144.4 0 0081.82-56.74c17 16.54 26.09 33.52 26.17 60.95a16.27 16.27 0 0015.1 16.37A16 16 0 00432 464c0-41.68-16.6-67.23-42.39-90.31A143.32 143.32 0 00400 320v-15.87h64a16 16 0 0016-16.77c-.42-8.61-7.84-15.23-16.45-15.23z" />
@@ -64,7 +65,7 @@ const Menu = (): JSX.Element => {
 
       <button
         onClick={() => setMenuOpened(!menuOpened)}
-        className="rounded-xl px-4 py-4 bg-white text-black focus:outline-none"
+        className="rounded-xl px-4 py-4 bg-white text-black menu-item focus:outline-none"
       >
         {!menuOpened ? (
           <svg
