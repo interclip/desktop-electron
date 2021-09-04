06cdaa688925d3616bcd78eebaf1a9994f6422c9
Filip Troníček <filip.tronicek@seznam.cz>
2021-09-04 22:52:00 +0200

    Update the logo

--- renderer/pages/about.tsx
+++ renderer/pages/about.tsx
@@ -26,7 +26,7 @@ const About = () : React.ReactFragment => {
       <div className="w-full h-screen">
         <Menu />
         <div className="flex flex-col content-center items-center text-white">
-        <img src="/images/logo.png" alt="Picture of the author" width={256} height={256} />
+        <img src="/images/logo.png" className="mt-5" alt="Interclip's logo" width={256} height={256} />
           <h1 className="text-5xl mt-12">About Interclip Desktop</h1>
           <div className="info text-xl mt-6 text-center">
             <ul>
